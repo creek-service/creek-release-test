db609622010aa6d9890640fc54ab2f48cd6695e5
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-11-02 15:44:59 +0000

    Release workflow

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -3,13 +3,12 @@ plugins {
 }
 
 val creekVersion : String by extra
-val creekTestVersion : String by extra
 val kafkaVersion : String by extra
 
 dependencies {
     implementation("org.creekservice:creek-service-context:$creekVersion")
 
-    testImplementation("org.creekservice:creek-kafka-streams-test:$creekTestVersion")
+    testImplementation("org.creekservice:creek-kafka-streams-test:$creekVersion")
 }
 
 // Patch Kafka Streams test jar into main Kafka Streams module to avoid split packages:
