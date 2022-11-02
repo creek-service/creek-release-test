5f2827eb7e9bfd2a5ce9e9e1803978ca85e9d3d9
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-11-02 10:59:21 +0000

    Split test from non-test creek dependencies

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     `java-library`
 }
+
+val creekVersion : String by extra
+val creekTestVersion : String by extra
+
+dependencies {
+    implementation("org.creekservice:creek-service-context:$creekVersion")
+
+    testImplementation("org.creekservice:creek-kafka-streams-test:$creekTestVersion")
+}
