8520f1659d3cf56ec55871edbe2ea38ef29161bf
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-11-03 21:49:57 +0000

    Looking good

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,18 @@ repositories {
     gradlePluginPortal()
 }
 
+val jvmTargetVer = 11;
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(jvmTargetVer))
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "$jvmTargetVer"
+    }
+}
+
 dependencies {
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")                           // https://plugins.gradle.org/plugin/io.github.gradle-nexus.publish-plugin
 }
