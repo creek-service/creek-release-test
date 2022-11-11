3c09c49f8c93c554af848ac1bc382f72ed44adf9
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-11-11 15:17:20 +0000

    Actually, let's keep GH packages support for now.
    Don't cost nuffin', and will allow development to go on if Maven is unavailable for a while.

--- buildSrc/src/main/kotlin/creek-common-convention.gradle.kts
+++ buildSrc/src/main/kotlin/creek-common-convention.gradle.kts
@@ -37,6 +37,9 @@ java {
 }
 
 repositories {
+    mavenCentral()
+
+    // Primary snapshot repo:
     maven {
         url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
         mavenContent {
@@ -45,7 +48,18 @@ repositories {
         }
     }
 
-    mavenCentral()
+    // Backup snapshot repo:
+    maven {
+        url = uri("https://maven.pkg.github.com/creek-service/*")
+        credentials {
+            username = "Creek-Bot-Token"
+            password = "\u0067hp_LtyvXrQZen3WlKenUhv21Mg6NG38jn0AO2YH"
+        }
+        mavenContent {
+            includeGroup("org.creekservice")
+            snapshotsOnly()
+        }
+    }
 }
 
 configurations.all {
